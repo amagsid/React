779c354faf80840f9ded2be1b7851641661817bf
Ahmad Magdy <amagsid@gmail.com>
2021-03-15 08:55:11 +0100

    added all weather info

--- Project/hackyourweather/src/components/WeatherInfo.js
+++ Project/hackyourweather/src/components/WeatherInfo.js
@@ -1,20 +1,41 @@
 const WeatherDescription = ({ city: { weather } }) => {
+  const main = weather[0].main;
+  const description = weather[0].description;
   return (
-    <div>
-      <h4>{weather[0].main}</h4>
-      <h5>{weather[0].description}</h5>
-    </div>
+    <>
+      <h3>{main}</h3>
+      <h4>{description}</h4>
+    </>
+  );
+};
+
+const TempInfo = ({
+  city: {
+    main: { temp_max },
+  },
+  city: {
+    main: { temp_min },
+  },
+}) => {
+  const toCelcius = (temp) => Math.floor(temp - 273.15);
+
+  return (
+    <>
+      <h4> min temp: {toCelcius(temp_min)}</h4>
+      <h4> max temp: {toCelcius(temp_max)}</h4>
+    </>
   );
 };
 
 const WeatherInfo = ({ city, name, country }) => {
   return (
-    <div>
-      <h3>
+    <>
+      <h2>
         {name}, {country}
-      </h3>
+      </h2>
       <WeatherDescription city={city} />
-    </div>
+      <TempInfo city={city} />
+    </>
   );
 };
 
