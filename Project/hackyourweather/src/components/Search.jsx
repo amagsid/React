87e782df4f423d339eb2273e17dc2877f0eb916e
Ahmad Magdy <amagsid@gmail.com>
2021-03-25 21:49:38 +0100

    refactored code with better error handling

--- Project/hackyourweather/src/components/Search.jsx
+++ Project/hackyourweather/src/components/Search.jsx
@@ -4,14 +4,12 @@ import Feedback from "./Feedback";
 import { FaSearch } from "react-icons/fa";
 
 const Search = () => {
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState(null);
   const [clickCount, setClickCount] = useState(0);
   const [weather, setWeather] = useState(null);
   const [isLoading, setLoading] = useState(false);
   // error handling
-  const [has400Error, set400Error] = useState(false);
-  const [has404Error, set404Error] = useState(false);
-  const didMount = useRef(false);
+  const [hasError, setError] = useState(null);
 
   const ApiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
 
@@ -22,70 +20,54 @@ const Search = () => {
       `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}`
     )
       .then((res) => {
-        if (!res.ok) {
-          if (res.status === 400)
-            throw new Error("Please enter a city to show weather");
-          else throw new Error("please enter a valid city name");
-        } else return res.json();
+        if (res.ok) return res.json();
+        else throw res;
       })
       .then((weather) => {
-        setLoading(true);
         setWeather(weather);
         setLoading(false);
       })
       .catch((err) => {
-        err === "Error: Please enter a city to show weather"
-          ? set400Error(true)
-          : set404Error(true);
+        console.error("error fetching data", err);
+        setError(err);
       })
       .finally(() => {
         setLoading(false);
       });
-    set400Error(false);
-    set404Error(false);
   };
 
-  //functionality//
-  // fetch useEffect. I used the useRef hook here because I wanted to prevent useEffect from running on first render since that way it always returned an error
+  //---functionality---//
+  // fetch useEffect
   useEffect(() => {
-    if (didMount.current) getWeather();
-    else didMount.current = true;
+    if (city) getWeather();
+    else setError({ status: 400 });
   }, [clickCount]);
 
-  //setTimeout on 400 error feedback
+  //setTimeout on  error feedback
   useEffect(() => {
     const timeout = setTimeout(() => {
-      set400Error(false);
+      setError(null);
     }, 2000);
 
     return () => clearTimeout(timeout);
-  }, [has400Error]);
-
-  //setTimeout on 404 error feedback
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      set404Error(false);
-    }, 2000);
-
-    return () => clearTimeout(timeout);
-  }, [has404Error]);
+  }, [hasError]);
 
   return (
     <div className="weather-app">
-      {/* error handling */}
-      {has400Error && (
+      {hasError && hasError.status === 404 && (
         <Feedback
           className={"feedbcak error"}
-          text={"Please provide a city to get weather data"}
+          text={"Please enter a valid city name"}
         />
       )}
-      {has404Error && (
+
+      {hasError && hasError.status === 400 && clickCount > 0 && (
         <Feedback
           className={"feedbcak error"}
-          text={"Please enter a valid city name "}
+          text={"Please provide a city name to get the weather results"}
         />
       )}
-      {/* check input. if false always show feedback to insert one, if true, ask user to click on button */}
+
       {city ? (
         <Feedback
           className={"feedbcak start"}
@@ -97,7 +79,6 @@ const Search = () => {
           text={"enter a city name to show weather "}
         />
       )}
-
       {/* //form */}
       <div className="weather-app-form">
         {!city ? <FaSearch className="search-icon" /> : null}
